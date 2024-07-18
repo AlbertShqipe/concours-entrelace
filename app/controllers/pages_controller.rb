4b7c8a4d8027fb9b418fbc38f6634680059181e3
Albert Nikolli <albert_nikolli@icloud.com>
2024-07-18 13:52:03 +0200

    added the controller actions, the routs and the vies for the different pages actions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,24 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :regulation, :media, :who_are_we, :practical_info, :contact, :our_partners ]
 
   def home
+  end
+
+  def regulation
+  end
+
+  def media
+  end
+
+  def who_are_we
+  end
+
+  def practical_info
+  end
+
+  def contact
+  end
+
+  def our_partners
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'regulation' => 'pages#regulation'
+  get 'media' => 'pages#media'
+  get 'who_are_we' => 'pages#who_are_we'
+  get 'practical_info' => 'pages#practical_info'
+  get 'contact' => 'pages#contact'
+  get 'our_partners' => 'pages#our_partners'
 
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
