5454e6430849555e1caaed5a784b4eb7c83872da
Albert Nikolli <albert_nikolli@icloud.com>
2024-07-31 07:42:45 +0200

    added the new logos and set the roles to the users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   enum role: { admin: 0, competitor: 1, jury: 2 }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
